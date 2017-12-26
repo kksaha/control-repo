13b85b0bc26aa54d6604ace63645a40fd675715f
root <root@puppet.codespaces.io>
2017-12-26 16:24:09 +0000

    creating spec for dev env

--- manifests/app.pp
+++ manifests/app.pp
@@ -1,4 +1,4 @@
-node 'node1' {
+node 'node3' {
 
   include base
   class {'::tomcat' :
@@ -16,33 +16,4 @@ node 'node1' {
   
 }
 
-node default {
 
-  notify { 'checkpoint_1' :
-    message => "
-    
-         CHECKPOINT_1
-         DEFAULT BLOCK APPLIED
-         Looks like there is no node defination for this host
-    "
-  }
-}
-
-
-node 'node2' {
-
-  include base
-  class {'::tomcat' :
-    xms           => '70m',
-    xmx           => '210m',
-    user          => 'tomcat',
-    group         => 'tomcat',
-    service_state => 'running',
-  }
-  
-  tomcat::deploy { "sysfoo":
-    deploy_url     => 'https://2-115020838-gh.circle-artifacts.com/0/tmp/circle-artifacts.rWYWrdH/sysfoo.war',
-    checksum_value => "28fc064d7acb8742e3eccd25bab78f81",
-  }
-  
-}
